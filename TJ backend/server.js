2da7deceb2444eaf76da53053cb591804f9467e5
Chizzy <ubakammachiedozie91@gmail.com>
2026-08-28 01:51:06 +0100

    port update

--- TJ backend/server.js
+++ TJ backend/server.js
@@ -46,6 +46,7 @@ app.use('/change-password', require('./routes/changePassword'));
 
 
 
-
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
+});
