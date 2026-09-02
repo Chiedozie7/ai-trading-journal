6d2e852df83cb49ad2635be8279c9b1c7b4b51cb
Chizzy <ubakammachiedozie91@gmail.com>
2026-09-02 02:09:20 +0100

    resend configuration

--- TJ backend/utils/sendEmail.js
+++ TJ backend/utils/sendEmail.js
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const sendEmail = async ({ to, subject, html }) => {
     try {
         const { data, error } = await resend.emails.send({
-            from: "onboarding@resend.dev",
+            from: "TradeLedger <noreply@tradeledger.cv>",
             to,
             subject,
             html,
