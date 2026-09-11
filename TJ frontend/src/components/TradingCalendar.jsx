243edee32e46f0874e060b6dbc430d7c13023e51
Chizzy <ubakammachiedozie91@gmail.com>
2026-09-11 22:47:40 +0100

    tweak

--- TJ frontend/src/components/TradingCalendar.jsx
+++ TJ frontend/src/components/TradingCalendar.jsx
@@ -174,7 +174,7 @@ function TradingCalendar({
                             {tradeDay && (
                                 <>
                                     <div className="day-pl">
-                                        ${tradeDay.netPL}
+                                        {tradeDay.netPL < 0 ? "-" : ""}${Math.abs(tradeDay.netPL)}
                                     </div>
 
                                     <div className="day-trades">
